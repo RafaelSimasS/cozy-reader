441ef330c300d876cc69740da9b9e0c8e903ef2a
RafaelSimasS <rafaeldesousasimas@gmail.com>
2023-07-19 16:50:39 -0300

    add: created modular component for popup

---
+++ app/components/CredentialsPopup/CredentialPopup.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { ReactNode, useState } from "react";
+
+import popupStyle from "./popup.module.css";
+
+import { Inter } from "next/font/google";
+import { Cairo } from "next/font/google";
+import { NextFont } from "next/dist/compiled/@next/font";
+const inter: NextFont = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+const cairo: NextFont = Cairo({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+import { Button } from "@/app/components/Button/Button";
+import buttonStyles from "@/styles/buttons.module.css";
+import Image from "next/image";
+
+type CredentialPopupProps = {
+  children?: ReactNode;
+  title: string,
+  isLoginButton: boolean,
+};
+
+
+export function CredentialPopup({ children, title, isLoginButton }: CredentialPopupProps) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleClick = () => {
+    setShowPopup(true);
+  };
+  const handleClose = () => {
+    setShowPopup(false);
+  };
+  return (
+    <>
+        <Button
+            className={`${buttonStyles.buttonSign} ${cairo.className} `}
+            onClick={handleClick}
+        >
+            {isLoginButton ? "Log In" : "Sign Up"}
+        </Button>
+        {showPopup && (
+            <div className={`overlay`}>
+            <div className={`${popupStyle.signUpContainer}`}>
+                <div className={`${popupStyle.row}`}>
+                <h2 className={`${inter.className} ${popupStyle.SignTitle}`}>
+                    {title}
+                </h2>
+                <Button
+                    className={`${buttonStyles.closeButton}`}
+                    onClick={handleClose}
+                >
+                    <Image
+                    src="/icons/close-round.svg"
+                    alt="Close"
+                    width={21}
+                    height={21}
+                    className={`${buttonStyles.closeIcon}`}
+                    />
+                </Button>
+                {children}
+                </div>
+            </div>
+            </div>
+        )}
+    </>
+  );
+}
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,13 +1,5 @@
 import Link from "next/link";
 
-import {
-  ButtonSign,
-  LoginButton,
-  LogoutButton,
-  ProfileButton,
-  RegisterButton,
-} from "./buttons.component";
-
 import utilsStyle from "../../styles/utils.module.css";
 
 import { Cairo } from "next/font/google";
@@ -17,8 +9,10 @@ const cairo: NextFont = Cairo({
   display: "swap",
 });
 
+import { CredentialPopup } from "./CredentialsPopup/CredentialPopup";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+
 export default async function Navbar() {
   const session = await getServerSession(authOptions);
   return (
@@ -27,18 +21,21 @@ export default async function Navbar() {
         COZY READER
       </Link>
       <div className={` ${utilsStyle.separateGap} ${utilsStyle.mR22} `}>
-        {/* <ButtonSign isSignUp={true} />
-        <ButtonSign isSignUp={false} /> */}
         {session ? (
-          <LogoutButton />
-          ) : (
-            <>
-            <LoginButton />
-            <RegisterButton />
+          <></>
+        ) : (
+          <>
+            <CredentialPopup
+              title="Log In"
+              isLoginButton={true}
+            ></CredentialPopup>
+            <CredentialPopup
+              title="Sign Up"
+              isLoginButton={false}
+            ></CredentialPopup>
           </>
         )}
       </div>
-      <img src="/github.svg" />
     </nav>
   );
 }
